2b2b7a3b517121ee77ef24039f376bfa64b7e87e
Jordan Castelloe <jordan.castelloe@gmail.com>
2018-01-31 10:38:51 -0600

    upload data to firebase and added firebase url and authentication

--- app/app.js
+++ app/app.js
@@ -6,6 +6,8 @@
 //   / forest /: id
 angular
   .module("NationalParks", ["ngRoute"])
+  .constant("FBUrl", "https://todo-app-42851.firebaseio.com/" // I reused my todo app database for this project so it's still called the todo app but it's the right data, I promise
+)
   .config($routeProvider => {
     $routeProvider
       .when("/register", {
@@ -30,4 +32,12 @@ angular
         controller: "ForestFavoritesCtrl",
       })
       .otherwise("/login");
+  })
+  .run(FBCreds => {
+    let creds = FBCreds;
+    let authConfig = {
+      apiKey: creds.key,
+      authDomain: creds.authDomain
+    };
+    firebase.initializeApp(authConfig);
   });
