331c83a69da937e41bbed38fee4aaa3faec4f77b
Jordan Castelloe <jordan.castelloe@gmail.com>
2018-01-31 12:57:13 -0600

    navbar works

--- app/controllers/forestListCtrl.js
+++ app/controllers/forestListCtrl.js
@@ -1,6 +1,13 @@
 'use strict';
 angular.module("NationalParks").controller("ForestListCtrl", function ($scope, ForestFactory) {
   $scope.test = "This is the forest list controller!";
+
+  $scope.addToFavorites = (forestId) => {
+    console.log("this should be a forest id", forestId);
+    // get current user
+    // pass into a patch method 
+  };
+
   ForestFactory.getForests()
   .then(forests => {
     $scope.forestList = forests;
---
+++ app/controllers/navCtrl.js
@@ -0,0 +1,27 @@
+'use strict';
+
+angular.module("NationalParks").controller("NavCtrl", function ($scope, $location) {
+  // $scope.isActive = (viewLocation) => viewLocation === $location.path();
+
+  $scope.navItems= [
+    {
+      name: "Logout",
+      url: "#!/logout"
+    },
+    {
+      name: "Login",
+      url: "#!/login",
+      bang: "!"
+    },
+    {
+      name: "Forests",
+      url: "#!/forests"
+    },
+    {
+      name: "My Favorits",
+      url: "#!/forests/favorites"
+    }
+  ];
+
+  console.log($scope.navItems);
+});
