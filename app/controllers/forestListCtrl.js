57a7033f72d3ef5d5cec3afa1f68db134d3c34b5
Jordan Castelloe <jordan.castelloe@gmail.com>
2018-01-31 10:58:48 -0600

    installed firebase (oops) and added script tag

--- app/controllers/forestListCtrl.js
+++ app/controllers/forestListCtrl.js
@@ -1,4 +1,9 @@
 'use strict';
-angular.module("NationalParks").controller("ForestListCtrl", function ($scope) {
+angular.module("NationalParks").controller("ForestListCtrl", function ($scope, ForestFactory) {
   $scope.test = "This is the forest list controller!";
+  ForestFactory.getForests()
+  .then(forests => {
+    console.log("forest data from within controller", forests);
+    // $scope.forestList = forests;
+  });
 });
